594d1ff68df2d15bf8264bca0b04fb2e33469f68
Ryan Kim <skim19@stuy.edu>
2019-04-10 11:14:16 -0400

    ,

--- draw.py
+++ draw.py
@@ -2,17 +2,14 @@ from display import *
 from matrix import *
 from gmath import *
 
-def scanline_convert(polygons, i, screen, zbuffer ):
-    pass
-
 def add_polygon( polygons, x0, y0, z0, x1, y1, z1, x2, y2, z2 ):
     add_point(polygons, x0, y0, z0)
     add_point(polygons, x1, y1, z1)
     add_point(polygons, x2, y2, z2)
 
-def draw_polygons( polygons, screen, zbuffer, color ):
+def draw_polygons( polygons, screen, color ):
     if len(polygons) < 2:
-        print 'Need at least 3 points to draw'
+        print('Need at least 3 points to draw')
         return
 
     point = 0
@@ -23,25 +20,19 @@ def draw_polygons( polygons, screen, zbuffer, color ):
         if normal[2] > 0:
             draw_line( int(polygons[point][0]),
                        int(polygons[point][1]),
-                       polygons[point][2],
                        int(polygons[point+1][0]),
                        int(polygons[point+1][1]),
-                       polygons[point+1][2],
-                       screen, zbuffer, color)
+                       screen, color)
             draw_line( int(polygons[point+2][0]),
                        int(polygons[point+2][1]),
-                       polygons[point+2][2],
                        int(polygons[point+1][0]),
                        int(polygons[point+1][1]),
-                       polygons[point+1][2],
-                       screen, zbuffer, color)
+                       screen, color)
             draw_line( int(polygons[point][0]),
                        int(polygons[point][1]),
-                       polygons[point][2],
                        int(polygons[point+2][0]),
                        int(polygons[point+2][1]),
-                       polygons[point+2][2],
-                       screen, zbuffer, color)
+                       screen, color)
         point+= 3
 
 
@@ -227,32 +218,31 @@ def add_curve( points, x0, y0, x1, y1, x2, y2, x3, y3, step, curve_type ):
         i+= 1
 
 
-def draw_lines( matrix, screen, zbuffer, color ):
+def draw_lines( matrix, screen, color ):
     if len(matrix) < 2:
-        print 'Need at least 2 points to draw'
+        print('Need at least 2 points to draw')
         return
 
     point = 0
     while point < len(matrix) - 1:
         draw_line( int(matrix[point][0]),
                    int(matrix[point][1]),
-                   matrix[point][2],
                    int(matrix[point+1][0]),
                    int(matrix[point+1][1]),
-                   matrix[point+1][2],
-                   screen, zbuffer, color)
+                   screen, color)    
         point+= 2
-
+        
 def add_edge( matrix, x0, y0, z0, x1, y1, z1 ):
     add_point(matrix, x0, y0, z0)
     add_point(matrix, x1, y1, z1)
-
+    
 def add_point( matrix, x, y, z=0 ):
     matrix.append( [x, y, z, 1] )
+    
 
 
 
-def draw_line( x0, y0, z0, x1, y1, z1, screen, zbuffer, color ):
+def draw_line( x0, y0, x1, y1, screen, color ):
 
     #swap points if going right -> left
     if x0 > x1:
@@ -267,55 +257,71 @@ def draw_line( x0, y0, z0, x1, y1, z1, screen, zbuffer, color ):
     y = y0
     A = 2 * (y1 - y0)
     B = -2 * (x1 - x0)
-    wide = False
-    tall = False
 
-    if ( abs(x1-x0) >= abs(y1 - y0) ): #octants 1/8
-        wide = True
-        loop_start = x
-        loop_end = x1
-        dx_east = dx_northeast = 1
-        dy_east = 0
-        d_east = A
-        if ( A > 0 ): #octant 1
+    #octants 1 and 8
+    if ( abs(x1-x0) >= abs(y1 - y0) ):
+
+        #octant 1
+        if A > 0:            
             d = A + B/2
-            dy_northeast = 1
-            d_northeast = A + B
-        else: #octant 8
-            d = A - B/2
-            dy_northeast = -1
-            d_northeast = A - B
 
-    else: #octants 2/7
-        tall = True
-        dx_east = 0
-        dx_northeast = 1
-        if ( A > 0 ): #octant 2
-            d = A/2 + B
-            dy_east = dy_northeast = 1
-            d_northeast = A + B
-            d_east = B
-            loop_start = y
-            loop_end = y1
-        else: #octant 7
-            d = A/2 - B
-            dy_east = dy_northeast = -1
-            d_northeast = A - B
-            d_east = -1 * B
-            loop_start = y1
-            loop_end = y
+            while x < x1:
+                plot(screen, color, x, y)
+                if d > 0:
+                    y+= 1
+                    d+= B
+                x+= 1
+                d+= A
+            #end octant 1 while
+            plot(screen, color, x1, y1)
+        #end octant 1
 
-    while ( loop_start < loop_end ):
-        plot( screen, zbuffer, color, x, y, 0 )
-        if ( (wide and ((A > 0 and d > 0) or (A < 0 and d < 0))) or
-             (tall and ((A > 0 and d < 0) or (A < 0 and d > 0 )))):
-
-            x+= dx_northeast
-            y+= dy_northeast
-            d+= d_northeast
+        #octant 8
         else:
-            x+= dx_east
-            y+= dy_east
-            d+= d_east
-        loop_start+= 1
-    plot( screen, zbuffer, color, x, y, 0 )
+            d = A - B/2
+
+            while x < x1:
+                plot(screen, color, x, y)
+                if d < 0:
+                    y-= 1
+                    d-= B
+                x+= 1
+                d+= A
+            #end octant 8 while
+            plot(screen, color, x1, y1)
+        #end octant 8
+    #end octants 1 and 8
+
+    #octants 2 and 7
+    else:
+        #octant 2
+        if A > 0:
+            d = A/2 + B
+
+            while y < y1:
+                plot(screen, color, x, y)
+                if d < 0:
+                    x+= 1
+                    d+= A
+                y+= 1
+                d+= B
+            #end octant 2 while
+            plot(screen, color, x1, y1)
+        #end octant 2
+
+        #octant 7
+        else:
+            d = A/2 - B;
+
+            while y > y1:
+                plot(screen, color, x, y)
+                if d > 0:
+                    x+= 1
+                    d+= A
+                y-= 1
+                d-= B
+            #end octant 7 while
+            plot(screen, color, x1, y1)
+        #end octant 7
+    #end octants 2 and 7
+#end draw_line
--- main.py
+++ main.py
@@ -5,15 +5,11 @@ from matrix import *
 import math
 
 screen = new_screen()
-zbuffer = new_zbuffer()
 color = [ 0, 255, 0 ]
 edges = []
 polygons = []
 t = new_matrix()
 ident(t)
 csystems = [ t ]
-
-
-parse_file( 'script', edges, polygons, csystems, screen, zbuffer, color )
-
-
+        
+parse_file( 'script', edges, polygons, csystems, screen, color )
